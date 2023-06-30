0cf5d4b3406130aff17c844854672288eb3d1dab
AnhNguyenTUDo <98350703+AnhNguyenTUDo@users.noreply.github.com>
2023-06-30 11:31:19 +0200

    updated pom.xml (WebApp-Project-presentation). Frontend and Backend can be packed together in .war file.

--- WebApp-Project-persistence/target/generated-sources/de/ls5/wt2/entity/DBMessage_.java
+++ WebApp-Project-persistence/target/generated-sources/de/ls5/wt2/entity/DBMessage_.java
@@ -8,8 +8,10 @@ import javax.persistence.metamodel.StaticMetamodel;
 @StaticMetamodel(DBMessage.class)
 public abstract class DBMessage_ extends de.ls5.wt2.entity.DBIdentified_ {
 
+	public static volatile SingularAttribute<DBMessage, DBUser> sender;
 	public static volatile SingularAttribute<DBMessage, String> content;
 
+	public static final String SENDER = "sender";
 	public static final String CONTENT = "content";
 
 }
